ff40ab0199c4444dbd8f21c3cfff085bc429104b
git-billy <100283578+git-billy@users.noreply.github.com>
2022-05-04 22:50:54 +0800

    update w11-sidebar app.js

--- demo/w11/sidebar-starter/app.js
+++ demo/w11/sidebar-starter/app.js
@@ -1,6 +1,14 @@
 const sidebarToggle = document.querySelector('.sidebar-toggle');
 const sidebar = document.querySelector('.sidebar');
 
+const closebar = document.querySelector('.close-btn');
+
+console.log(closebar);
+
 sidebarToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('show-sidebar');
+  sidebar.classList.toggle('show-sidebar');
+});
+
+closebar.addEventListener('click', () => {
+    sidebar.classList.remove('show-sidebar');
 });
