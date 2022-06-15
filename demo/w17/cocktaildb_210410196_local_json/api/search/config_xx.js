5a68c2bb22f675ec8889849d46890deb1a3220aa
git-billy <210410196@gms.tku.edu.tw>
2022-06-15 19:54:45 +0800

    w17-p2: fetch a local json data with local images

--- demo/w17/cocktaildb_210410196_local_json/api/search/config_xx.js
+++ demo/w17/cocktaildb_210410196_local_json/api/search/config_xx.js
@@ -2,4 +2,4 @@ export const searchItesm = ['h', 'apple', 'orange'];
 
 export const hasNetwork = false;
 
-export const LOCAL_URL = '/api/search/h.json';
+export const LOCAL_URL = '/api/search/orange.json';
